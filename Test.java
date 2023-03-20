82dfa0ed302ccc094509ddeeb72a7e604dc7f45a
katayama-Web <kairikatayama@gmail.com>
2023-03-20 05:08:01 +0000

    学習記録3/20

---
+++ Test.java
@@ -0,0 +1,19 @@
+package test55;
+
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello!!!");
+        System.out.println("World!!!");
+        System.out.println("計算をします");
+        System.out.println(30 + 33);
+        System.out.println("すがわら");
+        System.out.println("31歳です");
+        System.out.println("お酒が好きです");
+        System.out.println("31 + 31の計算をします");
+        System.out.println(31 + 31);
+        int x;
+        x = 6;
+        System.out.println(x * x * 3.14);
+    }
+}
+
